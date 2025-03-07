a15e3277d107ab9294b270372ca4eff855755419
David <pr0v0ziert@gmx.de>
2025-03-07 12:35:17 +0700

    Update [[...slug]].tsx

--- examples/with-next/pages/queuedash/[[...slug]].tsx
+++ examples/with-next/pages/queuedash/[[...slug]].tsx
@@ -1,16 +1,24 @@
 import { QueueDashApp } from "@queuedash/ui";
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
 
-function getBaseUrl() {
-  if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}/api/queuedash`;
-  }
-
-  return `http://localhost:${process.env.PORT ?? 3000}/api/queuedash`;
-}
-
-const Page: NextPage = () => {
-  return <QueueDashApp apiUrl={getBaseUrl()} basename="/queuedash" />;
+const Page: NextPage<{ url: string }> = ({ url }) => {
+  return <QueueDashApp apiUrl={url} basename="/queuedash" />;
 };
 
 export default Page;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  function getBaseUrl() {
+    if (process.env.VERCEL_URL) {
+      return `https://${process.env.VERCEL_URL}/api/queuedash`;
+    }
+
+    return `http://localhost:${process.env.PORT ?? 3000}/api/queuedash`;
+  }
+
+  return {
+    props: {
+      url: getBaseUrl(),
+    },
+  };
+};
